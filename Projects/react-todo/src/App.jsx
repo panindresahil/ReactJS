a3e268aee3e3a4dc311a76d5ba57fd2468c3c765
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2024-06-05 08:48:14 +0000

    Props

--- Projects/react-todo/src/App.jsx
+++ Projects/react-todo/src/App.jsx
@@ -4,13 +4,18 @@ import TodoItems from "./Components/TodoItems";
 import "./App.css"
 
 function App() {
+  let toDos = ["Work", "Read", "Walk"]
+  // let toDos= []
   return(
     <center className="todo-content">
       <Header/>
+      {toDos.length === 0 && <h1 style={{textAlign: "center"}}>No Work Items</h1>}
       <div className="container">
         <TodoInputControl/>
         <div className="items">
-          <TodoItems/>
+          {
+            toDos.map((item) => {return <TodoItems key={item} todoItem={item}/>})
+          }
         </div>
       </div>
     </center>
--- Projects/react-todo/src/Components/TodoItems.jsx
+++ Projects/react-todo/src/Components/TodoItems.jsx
@@ -1,12 +1,13 @@
-const TodoItems = () => {
+/* eslint-disable react/prop-types */
+const TodoItems = ({todoItem}) => {
     return(
         <>
         <div className="row m-2">
         <div className="col-6">
-          Buy Milk
+          {todoItem}
         </div>
         <div className="col-4">
-          6/3/2024
+          Date
         </div>
         <div className="col-2">
           <button className="btn btn-danger item-btn">Delete</button>
