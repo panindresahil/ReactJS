4337a7973dd3b02f4272c96fe7d55c063640f32b
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2024-06-07 11:21:54 +0000

    events

--- react-app/src/Components/FoodItems.jsx
+++ react-app/src/Components/FoodItems.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/prop-types */
 function FoodItems ({items}) {
+
+    const handleClick = (i) => {
+        alert(`${i} Added to Cart`)
+    }
+
     return(
         <>
-            <ul className="list-group my-3">
-                {items.map((i) => <li key={i} className="list-group-item">{i}</li>)}
+            <ul className="list-group grid text-center">
+                {items.map((i) => (<li key={i} className="list-group-item g-col-6">
+                    <span className="mx-5">{i}</span>
+                    <button className="button btn btn-success" onClick={() => {handleClick(i)}}>Add</button>
+                    </li>))}
             </ul>
         </>
     )
