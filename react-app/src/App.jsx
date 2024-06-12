94a6a4f4c08dc7154fe70e4103755c4753a8e909
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2024-06-12 05:45:58 +0000

    active

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,10 +3,10 @@ import Header from './Components/Header'
 import FoodItems from './Components/FoodItems'
 import Input from './Components/FoodInput'
 import { useState } from 'react'
-// import foodItems from './assets/data'
 
 function App() {
   const [searchFoodItem, setsearchFoodItem] = useState("")
+  const [bgColor, setBgColor] = useState("")
   const [foodItem, addFoodItem] = useState(["Rice", "Dal", "Roti"])
 
   const handleClick = (e) => {
@@ -20,6 +20,7 @@ function App() {
 
   const handleCart = (item) => {
     alert(`${item} has been added to cart.`);
+    setBgColor("active")
   }
 
   return (
@@ -28,7 +29,7 @@ function App() {
         <div className='container'>
           <Header/>
           <Input foodItem={searchFoodItem} handleKeyDown={handleClick} handleSearchChange={handleChange}/>
-          {foodItem.length === 0 ? <h1>No Items</h1> : <FoodItems items={foodItem}  handleClick={handleCart}/>}
+          {foodItem.length === 0 ? <h1>No Items</h1> : <FoodItems items={foodItem} color={bgColor}  handleClick={handleCart}/>}
         </div>
       </center>
     </>
--- react-app/src/Components/FoodItems.jsx
+++ react-app/src/Components/FoodItems.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-function FoodItems ({items, handleClick}) {
+function FoodItems ({items, handleClick, color}) {
 
     return(
         <>
             <ul className="list-group grid text-center">
-                {items.map((i) => (<li key={i} className="list-group-item g-col-6">
+                {items.map((i) => (<li key={i} className={`list-group-item g-col-6 ${color}`}>
                     <span className="mx-5">{i}</span>
                     <button className="button btn btn-success" onClick={() => handleClick(i)}>Add</button>
                     </li>))}
