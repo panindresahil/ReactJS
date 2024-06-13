512506c8f877bd6467abe488d5dcb465c7943b9e
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2024-06-13 08:04:18 +0000

    bg-changer complete

--- bgChanger-react/src/App.jsx
+++ bgChanger-react/src/App.jsx
@@ -3,26 +3,30 @@ import "./App.css"
 
 function App() {
 
-  const [color, setColor] = useState(["#365E32", "#E88D67", "#8E3E63"])
+  const [color, setColor] = useState("#FFF5E1")
 
-  const changeBgColor = () => {
-    const colors = ["#365E32", "#E88D67", "#8E3E63"]
-    const id = Math.round(Math.random() * 3) - 1
-    setColor(colors[id])
+  const changeBgColor = (col) => {
+
+    setColor(col)
   }
 
   return(
     <>
     <center>
-      <div className="container text-center" >
-        <h1 className="heading">BackGround Changer</h1>
-        <div className="row">
-        <div className="col box" style={{backgroundColor: color}} onClick={changeBgColor}>
-          BOX1
-        </div>
+      <div className="container text-center bg-container" style={{backgroundColor: color}}>
+      <h1 className="heading">BackGround Changer</h1>
+      <div className="row align-items-center">
+        <button type="button" onClick={() => changeBgColor("#1679AB")} className="col box btn btn-primary">Primary</button>
+        <button type="button" onClick={() => changeBgColor("#686D76")} className="col box btn btn-secondary">Secondary</button>
+        <button type="button" onClick={() => changeBgColor("#ADD899")} className="col box btn btn-success">Success</button>
+        <button type="button" onClick={() => changeBgColor("#C40C0C")} className="col box btn btn-danger">Danger</button>
+        <button type="button" onClick={() => changeBgColor("#FFC100")} className="col box btn btn-warning">Warning</button>
+        <button type="button" onClick={() => changeBgColor("#A3D8FF")} className="col box btn btn-info">Info</button>
+        <button type="button" onClick={() => changeBgColor("#FFF5E1")} className="col box btn btn-light">Light</button>
+        <button type="button" onClick={() => changeBgColor("#151515")} className="col box btn btn-dark">Dark</button>
         </div>
       </div>
-    </center>
+      </center>
     </>
   )
 }
