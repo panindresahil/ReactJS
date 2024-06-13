e64740f2ee91db7c1ac06f029dc9d18fcccd33f8
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2024-06-13 08:55:55 +0000

    new project

---
+++ react-pass-gen/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<div className="container text-center">
+  <div className="row">
+    <div className="col">
+      
+    </div>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
